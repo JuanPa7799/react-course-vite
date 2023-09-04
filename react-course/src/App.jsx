fe1e767b192bc3c88a5c71c43fa6c53d50db2b95
JuanPa7799 <garsa779999@gmail.com>
2023-09-03 19:37:29 -0600

    2.-Instalacion de React con Vite y TailwindCSS

---
+++ react-course/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='bg-blue-200'>
+        Hola Mundo
+      </div>
+    </>
+  )
+}
+
+export default App
