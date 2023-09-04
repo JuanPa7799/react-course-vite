c0233f568ba03f558e355433964535312321cc15
JuanPa7799 <garsa779999@gmail.com>
2023-09-04 09:35:05 -0600

    8.-Consumiendo la FakeStore API para pintar cards

--- react-course/src/Pages/Home/index.jsx
+++ react-course/src/Pages/Home/index.jsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from 'react'
 import React from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items,setItems] = useState(null);
+  useEffect(() => {
+    // fetch pide los elementos d ela API
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+
+  },[])
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid grap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+          <Card 
+            key ={item.id} 
+            data={item}
+          />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
