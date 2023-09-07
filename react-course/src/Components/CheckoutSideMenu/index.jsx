f4c85f1c103ee0482577e831432db79659f62984
JuanPa7799 <garsa779999@gmail.com>
2023-09-07 16:08:01 -0600

    18.-Evitando productos duplicados en el carrito.

--- react-course/src/Components/CheckoutSideMenu/index.jsx
+++ react-course/src/Components/CheckoutSideMenu/index.jsx
@@ -8,8 +8,6 @@ import './styles.css'
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
 
-  console.log('CART: ', context.cartProducts);
-
   return(
     <aside 
     className={`${context.isCheckoutSideMenuOpen ? 'flex ' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -21,7 +19,7 @@ const CheckoutSideMenu = () => {
           onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
       </div>
-      <div className='px-6'>
+      <div className='px-6 overflow-y-scroll'>
         {
           context.cartProducts.map(product => (
             <OrderCard 
