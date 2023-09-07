d11566b925aecfaed26f287344ca01ebec904164
JuanPa7799 <garsa779999@gmail.com>
2023-09-07 16:54:27 -0600

    19.-Eliminar productos del carrito.

--- react-course/src/Components/OrderCard/index.jsx
+++ react-course/src/Components/OrderCard/index.jsx
@@ -2,7 +2,7 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 
 
 const OrderCard = props => {
-  const {id, title, imageUrl, price } = props
+  const {id, title, imageUrl, price, handleDelete } = props
   return (
     <div className="flex justify-between items-center mb-4">
       <div className=' flex items-center gap-2'>
@@ -14,6 +14,7 @@ const OrderCard = props => {
       <div className='flex items-center gap-2'>
         <p className=' text-lg font-medium'>{price}</p>
         <XMarkIcon 
+          onClick={()=> handleDelete(id)}
           className=' h-6 w-6 text-black cursor-pointer'></XMarkIcon>
       </div>  
     </div>
