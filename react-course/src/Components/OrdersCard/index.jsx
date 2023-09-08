3d95fe56748784a5e6d8556cc5a3613ad7ee42c8
JuanPa7799 <garsa779999@gmail.com>
2023-09-08 15:44:24 -0600

    25.-Órdenes de compra con TailwindCSS

--- react-course/src/Components/OrdersCard/index.jsx
+++ react-course/src/Components/OrdersCard/index.jsx
@@ -1,15 +1,21 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts} = props
   return (
-    <div className="flex justify-between items-center mb-4 border border-black">
-      <p>
-        <span>01.02.23</span>  
-        <span>{totalProducts}</span>  
-        <span>{totalPrice }</span>  
-      </p> 
+    <div className="flex justify-between items-center border border-black p-4 w-80 rounded-lg mb-4">
+      <div className=' justify-between w-full'>
+        <p className='flex flex-col'>
+          <span className=' font-light'>01.02.23</span>  
+        </p>
+        <p className='flex items-center gap-2'>
+          <span className=' font-light'>{totalProducts} articles</span>  
+          <ChevronRightIcon 
+          className=' h-6 w-6 text-black cursor-pointer'/>
+        </p>
+        <span className=' font-medium text-2xl'>${totalPrice }</span>  
+      </div> 
     </div>
   )
 }
